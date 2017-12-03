998319f5a4a3b47f1ef37042a9996fe428ec8e1d
Marvinmw <1300623733@qq.com>
2017-12-03 18:27:39 +0100

    delete

--- logger/BatchLosses.py
+++
@@ -1,9 +0,0 @@
-import keras
-class LossHistory(keras.callbacks.Callback):
-    def on_train_begin(self, logs={}):
-        self.losses = []
-        self.acc = []
-
-    def on_batch_end(self, batch, logs={}):
-        self.losses.append(logs.get('loss'))
-        self.acc.append(logs.get('acc'))
